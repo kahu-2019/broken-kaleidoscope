927a073e3e8374edefb44f4ab04a8dfa59ae3e16
student <you@eda.com>
2019-01-29 10:31:18 +1300

    created a pixel

---
+++ client/components/Pixel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pixel = () => {
+  return <div style={{
+    height: '200px',
+    width: '200px',
+    backgroundColor: '#a7f1f9'
+
+  }}></div>
+}
+
+export default Pixel
