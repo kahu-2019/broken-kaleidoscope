c79a6f32e16b198e9a2e3252b31f1de5132c381e
Arpo <a.vandererf@gmail.com>
2019-01-29 11:17:46 +1300

    mvp working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Pixel from './Pixel'
 
 const App = () => {
-  return (
-    <div>Yup.</div>
-  )
+
+  var pixels = []
+  for (var i = 0; i < 5000; i++) {
+    pixels.push(<Pixel key={i} />)
+  }
+  return pixels
 }
 
 export default App
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class Pixel extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.randomHexColor = () =>
+      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+    this.state = {
+      style: {
+        height: '20px',
+        width: '20px',
+        backgroundColor: this.randomHexColor()
+      }
+    }
+  }
+
+  clickHandler(evt) {
+    this.setState({
+      style: { height: '20px', width: '20px', backgroundColor: this.randomHexColor() }
+    })
+  }
+
+  render() {
+    return (
+      <div style={this.state.style} onMouseOver={this.clickHandler.bind(this)}></div>
+    )
+  }
+}
+
+export default Pixel
