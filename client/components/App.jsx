ed5b99d927b1dce301dc28e956f0319d0d0170bf
student <mukherjeei@outlook.co.nz>
2019-01-29 11:25:53 +1300

    adding pixel class

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
+  var pixels = []
+  for (var i = 0; i < 5618; i++) {
+    pixels.push(<Pixel />)
+  }
+
   return (
     <div>
-      <Pixel />
+      {pixels}
     </div>
   )
 }
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 
-const pixelStyle = {
-    height: '20px',
-    width: '20px',
-    backgroundColor: 'aqua',
+// const pixelStyle = {
+//     height: '20px',
+//     width: '20px',
+//     backgroundColor: 'aqua',
+// }
+
+
+class Pixel extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            height: '20px',
+            width: '20px',
+            backgroundColor: 'aqua'
+        }
+    }
+    render() {
+        const pixelStyle = {
+            height: this.state.height,
+            width: this.state.width,
+            backgroundColor: this.state.backgroundColor,
+        }
+        return (
+            <div style={pixelStyle}></div>
+        )
+    }
 }
 
 
-const Pixel = () => {
-    return (
-        <div style={pixelStyle}></div>
-    )
-}
+// const Pixel = () => {
+//     return (
+//         <div style={pixelStyle}></div>
+//     )
+// }
+
 
 export default Pixel
