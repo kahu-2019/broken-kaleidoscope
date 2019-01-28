92442fa0e9aac248102a3bfe1de09eb0f4ef1a78
Alex <kuan8@hotmail.com>
2019-01-29 10:23:14 +1300

    added style to div

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Pixel from './Pixel'
 
 const App = () => {
   return (
-    <div>Yup.</div>
+    <Pixel />
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pixel = () => {
+  return (<div style={{
+    fontFamily: 'Times New Roman',
+    height: '50px',
+    width: '50%',
+    backgroundColor: 'blue'
+  }}></div>)
+}
+
+
+export default Pixel 
