f0ba6dc4fc06ab3bdd50d01278fe6a74401ec17d
Alex <kuan8@hotmail.com>
2019-01-29 10:59:32 +1300

    set initial state done

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (<div style={{
-    fontFamily: 'Times New Roman',
-    height: '50px',
-    width: '5%',
-    backgroundColor: 'blue'
-  }}></div>)
+class Pixel extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      style: {
+        fontFamily: 'Times New Roman',
+        height: '50px',
+        width: '5%',
+        backgroundColor: 'blue'
+      },
+      show: true
+    }
+
+  }
+
+  render() {
+    return <div style={this.state.style}></div>
+  }
+
 }
 
 
